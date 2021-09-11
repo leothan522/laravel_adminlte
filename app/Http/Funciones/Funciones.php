28e14b933dd6f354ba39a336a960a9042f03ac77
Yonathan Castillo <leothan522@gmail.com>
2021-09-11 08:21:50 -0400

    first commit

---
+++ app/Http/Funciones/Funciones.php
@@ -0,0 +1,7 @@
+<?php
+
+function hola(){
+    return "Funciones Funcionando correctamente";
+}
+
+?>
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,29 @@
+@extends('adminlte::page')
+
+{{--@section('title', 'Dashboard')--}}
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@endsection
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+    <p>{{ hola() }}</p>
+    <p>
+        <button onclick="Swal.fire('Any fool can use a computer')">hola</button></p>
+@endsection
+
+@section('footer')
+    hola
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@endsection
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@endsection
+
+@section('plugins.Sweetalert2', true)
+@section('plugins.Pace', true)
